cb81c3860edf2b7851a4a79d8578c71450746922
suchaimi <suchaimi.afif@gmail.com>
2019-12-20 04:31:59 +0700

    Logging status when starting server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"github.com/suchaimi/finance/internal/api"
+	"github.com/suchaimi/finance/internal/config"
+)
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.WithField("Version", config.Version).Debug("starting server.")
+
+	router, err := api.NewRouter()
+	if err != nil {
+		logrus.WithError(err).Fatal("Error building website router")
+	}
+
 	const addr = "0.0.0.0:8088"
 	server := http.Server{
-		Addr: addr,
+		Addr:    addr,
+		Handler: router,
 	}
 }
